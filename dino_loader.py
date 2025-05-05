bb0cc0c99869408d45526ae80eea7ea6f15959cc
XenoverseUp <xenoverseup@gmail.com>
2025-05-05 17:39:56 +0300

    Co-authored-by: RoshaRazmarafar02 <RoshaRazmarafar02@users.noreply.github.com>

--- dino_loader.py
+++ dino_loader.py
@@ -1,9 +1,8 @@
 import torch
-from torchvision.models.vision_transformer import vit_b_16, ViT_B_16_Weights
 
 def load_dino_model():
-    weights = ViT_B_16_Weights.IMAGENET1K_SWAG_E2E_V1
-    model = vit_b_16(weights=weights)
-    model.eval()
-    model.token_dim = model.heads.head.in_features
+    model = torch.hub.load('facebookresearch/dino:main', 'dino_vitb8')
+
+    model.eval()  
+    model.token_dim = 768 
     return model
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
+
 if __name__ == "__main__":
-    from train import main
-    main()
+    pass
